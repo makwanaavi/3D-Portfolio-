4d9107585c0da64c33fc644c059e6190e530f450
makwanaavi <makwanaavi73@gmail.com>
2025-06-28 13:45:53 +0530

    #UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import About from "./sections/About";
+import Experiences from "./sections/Experiences";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Projects from "./sections/Projects";
@@ -10,7 +11,7 @@ function App() {
       <Hero/>
       <About/>
       <Projects/>
-   
+      <Experiences/>
     </div>
   </>;
 }
