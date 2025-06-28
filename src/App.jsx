46e24bbcbf39f0440dc01f589a80ece70c531bdf
makwanaavi <makwanaavi73@gmail.com>
2025-06-28 15:35:07 +0530

    #update

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Experiences from "./sections/Experiences";
 import Testimonial from "./sections/Testimonial";
-// import Contact from "./sections/Contact";
-// import Footer from './sections/Footer';
+import Contact from "./sections/Contact";
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
@@ -17,8 +17,8 @@ const App = () => {
       <Projects />
       <Experiences />
       <Testimonial />
-      {/* <Contact />
-      <Footer/> */}
+      <Contact />
+      <Footer/>
     </div>
   );
 };
